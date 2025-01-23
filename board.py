d2a93a461d4547dc6d4dba2fc37fcfe65c82fcaf
Tristankn <tristan.natvig@gmail.com>
2025-01-23 11:41:20 +0100

    23_01

--- board.py
+++ board.py
@@ -8,17 +8,30 @@ class Board:
         # Nums parameter is a 2D list, like what the sudoku_reader returns
         self.n_rows = len(nums[0])
         self.n_cols = len(nums)
+        self.rawnums = nums
         self.nums = [[None for _ in range(self.n_rows)] for _ in range(self.n_cols)]
 
-    def _set_up_nums(self, board):
+    def _set_up_nums(self):
         # Denne funksjonen må hente tall fra sudokureader og putte dem inn i stedet for None
-        number_list = board
         
+        for i in range(len(self.nums)):
+            for j in range(len(self.nums[i])):
+                self.nums[i][j] = Square(i, j, self.rawnums[i][j])
 
-    def _set_up_elems(self):
-        # You should set up links between your squares and elements
-        # (rows, columns, boxes)
-        pass
+            
+    def _set_up_elems(self, squareboard):
+        
+        x = []
+        y = []
+    
+        for b_row in squareboard:
+            for b_column in b_row:
+
+
+
+
+        
+        
 
     def solve(self):
         # Your solving algorithm goes here!
@@ -39,23 +52,56 @@ class SudokuBoard(Board):
     pass
 
 class Square:
+
+    list = []
     
-    def __init__(self):
-        number = 0
-        row = 0
-        collumn = 0
-        box = 0
+    def __init__(self, row, column, value):
+        self.row = row
+        self.column = column
+        self.value = value
+        Square.list.append(self)
+
+    def print_list(self):
+        for item in Square.list:
+            print(item.value)
+
+class Elements:
+    def __init__(self, coords, type):
+        self.coords = coords
+        self.type = type
+
+        if type == "element_row":
+            print("Type is row")
+        if type == "element_column":
+            print("Type is column")
+        if type == "element_box":
+            print("Type is box")
+
+    def legal_value(self):
+        pass
+
+        
 
 
 if __name__ == "__main__":
     # Test code...
-    square = Square()
    
     reader = Sudoku_reader("sudoku_10.csv")
     board = Board(reader.next_board())
     print(board)
-
     print("NY KJØRING")
+    
+
+    #firkant = Square(1, 1, 10)
+    squareboard = board._set_up_nums()
+    
+    ex_row = Elements(1, "element_row")
+    ex_col = Elements(1, "element_column")
+    ex_box = Elements(1, "element_box")
+    #Square.print_list(list)
+    
+
+    #board._set_up_nums(board)
 
     #print(square)
     
